96d9263e39423db88e7508449350118a724d9b9a
iashchuk <221box@list.ru>
2018-08-01 23:30:34 +0300

    Добавлен JavaScript для модального окна с картой

---
+++ js/map.js
@@ -0,0 +1,23 @@
+var mapLink = document.querySelector(".button--map");
+
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal__close");
+
+mapLink.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
